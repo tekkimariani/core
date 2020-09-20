2ddee918e0b3f24779399f2cb2b13b78d8dbab3f
tekki <tekki.mariani@googlemail.com>
2020-09-20 20:35:55 +0200

    testing trait

--- src/page/html/AttrSrc.php
+++ src/page/html/AttrSrc.php
@@ -1,9 +1,7 @@
 <?php
 namespace tekki\core\page\html;
-use HTMLAttr as Attr;
 trait AttrSrc{
-    //public const SRC = 'src';
     public function attrSrc($url){
-        $this->appendAttribut([Attr::SRC => $url]);
+        $this->appendAttribut(['src' => $url]);
     }
 }
