006fd2af48779a01de4f8056e4053d4350cd4334
tekki <tekki.mariani@googlemail.com>
2020-09-21 02:45:38 +0200

    test

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ use tekki\core\page\html\HTML;
 $html = new HTML();
 $body = $html->body();
 $img = $body->img();
-$img->attrSrc('pic/ohne_gentechnik.jpg');
+$img->setSrc('pic/ohne_gentechnik.jpg');
 // $img->attrWidth('100px');
 // $img->attrHeight('100px');
 // $img->attrAlt('Bioland');
