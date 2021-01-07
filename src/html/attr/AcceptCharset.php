2438cd6dc138af551339097ce15991e6567cb5aa
tekki mariani <46289163+tekkimariani@users.noreply.github.com>
2021-01-07 16:09:30 +0100

    Delete AcceptCharset.php

--- src/html/attr/AcceptCharset.php
+++
@@ -1,7 +0,0 @@
-<?php
-namespace tekki\core\html\attr;
-trait Acceptcharset {
-    public function setAcceptcharset(string $value){
-        return $this->appendAttribut([Attr::ACCEPT_CHARSET => $value]);
-    }
-}
