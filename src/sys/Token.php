215d2fc5468034313a72ac56d4ce6d1d420d570f
tekki <tekki.mariani@googlemail.com>
2021-01-10 18:57:30 +0100

    ..

--- src/sys/Token.php
+++ src/sys/Token.php
@@ -1,13 +1,12 @@
 <?php
 namespace tekki\core\sys;
-class Token{
-    public static function generate(){
-        return Session::put(Config::get('session/token_name'), \md5(\uniqid()));
+class Token {
+    public static function generate() {
+        return Session::put(Config::get('session/token'), \md5(\uniqid()));
     }
-    
-    public static function check($token){
-        $tokenName = Config::get('session/token_name');
-        if(Session::exists($tokenName) && $token === Session::get($tokenName)){
+    public static function check($token) {
+        $tokenName = Config::get('session/token');
+        if (Session::exists($tokenName) && $token === Session::get($tokenName)) {
             Session::delete($tokenName);
             return true;
         }
