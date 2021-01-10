d342466d57e5cd676cf61ec03f6003443cd04847
tekki <tekki.mariani@googlemail.com>
2021-01-10 17:15:12 +0100

    ..

---
+++ src/sys/Validate.php
@@ -0,0 +1,64 @@
+<?php
+namespace tekki\core\sys;
+class Validate {
+    private $_passed = false,
+            $_errors = array(),
+            $_db = null;
+    public function __construct(){
+        $this->_db = DB::getInstance();
+    }
+    public function check($source, $items = array()) {
+        foreach ($items as $item => $rules) {
+            foreach ($rules as $rule => $rule_value) {
+                $value = $source[$item];
+                $item = htmlentities($item, ENT_QUOTES, 'UTF-8');
+                if ($rule === 'required' && empty($value)) {
+                    $this->addError("{$item} is required");
+                } else if (!empty($value)) {
+                    switch ($rule) {
+                        case 'min':
+                            if (strlen($value) < $rule_value) {
+                                $this->addError("{$item} must be a minimum of {$rule_value} characters.");
+                            }
+                            break;
+                        case 'max':
+                            if (strlen($value) > $rule_value) {
+                                $this->addError("{$item} must be a maximum of {$rule_value} characters.");
+                            }
+                            break;
+                        case 'matches':
+                            if ($value != $source[$rule_value]) {
+                                $this->addError("{$rule_value} must match {$item}.");
+                            }
+                            break;
+                        case 'unique':
+                            $check = $this->_db->get($rule_value, array($item, '=',$value));
+                            if ($check->count()) {
+                                $this->addError("{$item} already exists.");
+                            }
+                            break;
+                        case 'exists':
+                            $check = $this->_db->get($rule_value, array($item, '=',$value));
+                            if (!$check->count()) {
+                                $this->addError("{$item} do not exists.");
+                            }
+                            break;
+                    }
+                }
+            }
+        }
+        if (empty($this->_errors)) {
+            $this->_passed = true;
+        }
+        return $this;
+    }
+    private function addError($error){
+        $this->_errors[] = $error;
+    }
+    public function errors(){
+        return $this->_errors;
+    }
+    public function passed(){
+        return $this->_passed;
+    }
+}
